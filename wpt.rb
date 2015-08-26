f31ce28c28ac0f0d21884b072483758035c1136e
Guillermo Esteves <g@gesteves.com>
2015-08-26 14:47:14 -0400

    Update wpt.rb

--- wpt.rb
+++ wpt.rb
@@ -29,7 +29,7 @@ class WPT
     if active_test? && !test_complete?
       puts 'WPT request skipped; last test not complete'
     else
-      url = "http://www.webpagetest.org/runtest.php?url=#{@url}&k=#{@key}&f=json"
+      url = "http://www.webpagetest.org/runtest.php?url=#{@url}&location=#{ENV['WPT_LOCATION']}&k=#{@key}&f=json"
       request = HTTParty.get(url)
       response = JSON.parse(request.body)
       if response['statusCode'] == 200
