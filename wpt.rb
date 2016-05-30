e4e17609af9d2f0d7c75c7c1b8490e236bec1d81
Guillermo Esteves <g@gesteves.com>
2016-05-30 10:15:25 -0400

    Log in librato

--- wpt.rb
+++ wpt.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require 'redis'
 require 'dotenv'
-require 'socket'
+require 'librato/metrics'
 
 class WPT
   def initialize(url, key)
@@ -29,7 +29,7 @@ class WPT
     if active_test? && !test_complete?
       puts 'WPT request skipped; last test not complete'
     else
-      url = "http://www.webpagetest.org/runtest.php?url=#{@url}&location=#{ENV['WPT_LOCATION']}&k=#{@key}&f=json"
+      url = "http://www.webpagetest.org/runtest.php?url=#{@url}&location=#{ENV['WPT_LOCATION']}&k=#{@key}&f=json&runs=#{ENV['WPT_RUNS']}"
       request = HTTParty.get(url)
       response = JSON.parse(request.body)
       if response['statusCode'] == 200
@@ -46,48 +46,47 @@ class WPT
   end
 
   def log_results
-    if !ENV['HOSTEDGRAPHITE_APIKEY'].nil? && active_test? && test_complete?
+    if active_test? && test_complete?
 
       wpt = get_latest_result
 
-      key = ENV['HOSTEDGRAPHITE_APIKEY']
-      conn = TCPSocket.new 'carbon.hostedgraphite.com', 2003
-      metrics = ''
+      Librato::Metrics.authenticate ENV['LIBRATO_USER'], ENV['LIBRATO_TOKEN']
 
-      unless wpt['data']['runs']['1']['firstView'].nil?
-        first_view = wpt['data']['runs']['1']['firstView']
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.speedindex #{first_view['SpeedIndex']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.timings.ttfb #{first_view['TTFB']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.timings.doc_complete #{first_view['docTime']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.timings.fully_loaded #{first_view['fullyLoaded']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.timings.visually_complete #{first_view['visualComplete']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.timings.render_start #{first_view['render']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.bytes.in #{first_view['bytesIn']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.bytes.in_doc #{first_view['bytesInDoc']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.dom_elements #{first_view['domElements']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.responses.200 #{first_view['responses_200']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.responses.404 #{first_view['responses_404']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.first_view.responses.other #{first_view['responses_other']}\n"
+      queue = Librato::Metrics::Queue.new
+
+      unless wpt['data']['median']['firstView'].nil?
+        first_view = wpt['data']['median']['firstView']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.speedindex" => first_view['SpeedIndex']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.timings.ttfb" => first_view['TTFB']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.timings.doc_complete" => first_view['docTime']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.timings.fully_loaded" => first_view['fullyLoaded']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.timings.visually_complete" => first_view['visualComplete']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.timings.render_start" => first_view['render']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.bytes.in" => first_view['bytesIn']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.bytes.in_doc" => first_view['bytesInDoc']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.dom_elements" => first_view['domElements']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.responses.200" => first_view['responses_200']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.responses.404" => first_view['responses_404']
+        queue.add "#{ENV['PREFIX']}.wpt.first_view.responses.other" => first_view['responses_other']
       end
 
-      unless wpt['data']['runs']['1']['repeatView'].nil?
-        repeat_view = wpt['data']['runs']['1']['repeatView']
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.speedindex #{repeat_view['SpeedIndex']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.timings.ttfb #{repeat_view['TTFB']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.timings.doc_complete #{repeat_view['docTime']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.timings.fully_loaded #{repeat_view['fullyLoaded']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.timings.visually_complete #{repeat_view['visualComplete']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.timings.render_start #{repeat_view['render']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.bytes.in #{repeat_view['bytesIn']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.bytes.in_doc #{repeat_view['bytesInDoc']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.dom_elements #{repeat_view['domElements']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.responses.200 #{repeat_view['responses_200']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.responses.404 #{repeat_view['responses_404']}\n"
-        metrics += key + ".#{ENV['PREFIX']}.wpt.repeat_view.responses.other #{repeat_view['responses_other']}\n"
+      unless wpt['data']['median']['repeatView'].nil?
+        repeat_view = wpt['data']['median']['repeatView']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.speedindex" => repeat_view['SpeedIndex']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.timings.ttfb" => repeat_view['TTFB']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.timings.doc_complete" => repeat_view['docTime']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.timings.fully_loaded" => repeat_view['fullyLoaded']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.timings.visually_complete" => repeat_view['visualComplete']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.timings.render_start" => repeat_view['render']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.bytes.in" => repeat_view['bytesIn']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.bytes.in_doc" => repeat_view['bytesInDoc']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.dom_elements" => repeat_view['domElements']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.responses.200" => repeat_view['responses_200']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.responses.404" => repeat_view['responses_404']
+        queue.add "#{ENV['PREFIX']}.wpt.repeat_view.responses.other" => repeat_view['responses_other']
       end
 
-      conn.puts metrics
-      conn.close
+      queue.submit
     end
   end
 end
